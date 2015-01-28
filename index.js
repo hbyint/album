11e4c0e458b4af2eefebbb7f0c16b91574c98b59
하병용 <hbyint@gmail.com>
2015-01-28 11:01:39 +0900

    앨범 등록 기능 추가

--- index.js
+++ index.js
@@ -9,31 +9,36 @@ $(document).ready(function() {
 			self.makeDropDown();
 
 			// 모든 버튼 disable
-			$('.btn').addClass('disabled');
+			$('div[name=songManager] .btn').addClass('disabled');
 		}
 
 		// 이벤트 바인드 처리 함수
 		, eventBinding : function () {
 			$('#albumList').on('click', 'li', function() {
 				if ($(this).hasClass('new')) {
+					$('#infoForm .form-control').val('');
 					$('#infoArea').hide();
 					$('#infoForm').show();
 
-					$('button[name=regAlbum]').removeClass('disabled');
+					$('button[name=savAlbum').hide();
+					$('button[name=delAlbum').hide();
 
 					self.currentAlbum = {};
 					self.deleteSongAll();
+
+					$('div[name=albumManager] .btn').hide();
 				} else {
 					var idx = $.inArray(this, $('#albumList li'));
 
 					self.currentAlbum = self.dataSource[idx];
-					console.log(self.currentAlbum);
 					self.renderAlbum();
 					self.renderSongList();
 					self.btnRelease();
 
 					$('#infoForm').hide();
 					$('#infoArea').show();
+
+					$('div[name=albumManager] .btn').show();
 				}
 
 				$('.dropdown-toggle span[name=selectedTitle]').text($(this).find('a').text());
@@ -46,9 +51,38 @@ $(document).ready(function() {
 				$('#infoForm').hide();
 				$('#infoArea').show();
 				self.makeDropDown();
-				self.renderAlbum();
+				//self.renderAlbum();
+				self.selectAlbum();
 
 				return false;
+			});
+
+			$('a[name="modAlbum"]').click(function() {
+				$('#infoArea').hide();
+				$('#infoForm').show();
+				$('button[name=regAlbum').hide();
+				$('button[name=savAlbum').show();
+				$('button[name=delAlbum').show();
+
+				var album = self.currentAlbum.album;
+
+				$('input[name=albumTitle]').val(album.title);
+				$('input[name=albumArtist]').val(album.artist);
+				$('input[name=albumImg]').val(album.img);
+				$('input[name=albumGenre]').val(album.genre);
+				$('input[name=albumInfo]').val(album.info);
+				$('input[name=albumPublish]').val(album.publish);
+			});
+
+			$('button[name=savAlbum').click(function() {
+				self.updateAlbum();
+				$('#infoArea').hide();
+				$('#infoForm').show();
+				self.renderAlbum();
+			});
+
+			$('button[name=delAlbum').click(function() {
+
 			});
 
 			$('a[name=addSong]').click(function() {
@@ -94,6 +128,23 @@ $(document).ready(function() {
 			$('<li/>').wrapInner('<a>'+album.title+'</a>').appendTo('#albumList');
 		}
 
+		, updateAlbum : function () {
+			self.currentAlbum.album.title 	= $('input[name=albumTitle]').val();
+			self.currentAlbum.album.artist 	= $('input[name=albumArtist]').val();
+			self.currentAlbum.album.img 		= $('input[name=albumImg]').val();
+			self.currentAlbum.album.genre 	= $('input[name=albumGenre]').val();
+			self.currentAlbum.album.info 		= $('input[name=albumInfo]').val();
+			self.currentAlbum.album.publish = $('input[name=albumPublish]').val();
+		}
+
+		, deleteAlbum : function (album) {
+
+		}
+
+		, selectAlbum : function(no) {
+			
+		}
+
 		// 모든 버튼의 disable을 해제한다.
 		, btnRelease : function() {
 			$('a.btn').removeClass('disabled');
@@ -105,8 +156,7 @@ $(document).ready(function() {
 
 		// 앨범을 선택했을때 앨범정보 화면을 생성하는 함수
 		, renderAlbum : function () {
-			var album = self.currentAlbum;
-
+			var album = self.currentAlbum.album;
 			var html = '<img src="' + album.img + '">\n' +
 					       '  <div class="caption">\n' +
 					       '  <p style="font-size: 16px;"><strong>아티스트</strong> <span>' + album.artist +'</span></p>' +
